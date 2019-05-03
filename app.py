8b9bca47610023ef83d20f11aa62878b4d2f189e
Sara <sdkevorkian@gmail.com>
2019-05-03 09:44:12 -0700

    updating routes put and patch

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ books = [
         'name': 'The Cat in the Hat',
         'price': 6.99,
         'isbn': 9782371000193
+    },
+        {
+        'name': 'harry potter and the lower case letters',
+        'price': 16.99,
+        'isbn': 9782371002424
     }
 ]
 
@@ -68,5 +73,38 @@ def get_book_by_isbn(isbn):
             }
     return jsonify(return_value)
 
+#PUT - replace a book, requires all data
+@app.route('/books/<int:isbn>', methods=['PUT'])
+def replace_book(isbn):
+    request_data = request.get_json()
+    new_book = {
+        'name': request_data['name'],
+        'price': request_data['price'],
+        'isbn': isbn
+    }
+    i = 0
+    for book in books:
+        currentIsbn=book['isbn']
+        if currentIsbn == isbn:
+            books[i] = new_book
+        i += 1
+    response = Response("", status=204)
+    return response
+
+#PATCH - only update 1 piece of data on object
+@app.route('/books/<int:isbn>', methods=['PATCH'])
+def update_book(isbn):
+    request_data = request.get_json()
+    updated_book = {}
+    if("name" in request_data):
+        updated_book['name'] = request_data['name']
+    if("price" in request_data):
+        updated_book['price'] = request_data['price']
+    for book in books:
+        if book['isbn'] == isbn:
+            book.update(updated_book)
+    response = Response('', status=204)
+    response.headers['Location'] = '/book/' + str(isbn)
+    return response
 
 app.run(port=5000)
