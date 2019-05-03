d671efe6b840a19dac63bbb284fa65839d22f083
Sara <sdkevorkian@gmail.com>
2019-05-03 09:20:45 -0700

    added a post route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response, json
 
 app = Flask(__name__) 
 #Flask knows where to look for templates, static files, packages and so on
@@ -23,13 +23,38 @@ books = [
 def get_books():
     return jsonify({'books': books})
 
+def validBookObject(bookObject):
+    # if "keyName" in dictionaryObject
+    if ("name" in bookObject and "price" in bookObject and "isbn" in bookObject):
+        return True
+    else:
+        return False
 
 #POST /books
 #need to have request with details of book in body
 #it will be a JSON  - need to get request body
 @app.route('/books', methods=['POST'])
 def add_book():
-    return jsonify(request.get_json())
+    request_data  = request.get_json()
+    if(validBookObject(request_data)):
+        new_book= {
+            "name": request_data["name"],
+            "isbn": request_data["isbn"],
+            "price": request_data["price"],
+        }
+        books.insert(0, new_book)
+        #in FLASK when return string automatically set contenttype to text/html and status to 200
+        response = Response("", 201, mimetype='application/json')
+        response.headers['Location'] = '/books/' + str(new_book['isbn'])
+        return response
+    else:
+        invalidBookObjectErrorMsg = {
+            "error": "Invalid book object passed in request.",
+            "helpString": "Data should be passed similar to this: {'name': 'bookname', 'price': '7.99', 'isbn':'1234567890'}"
+        }
+        response = Response(json.dumps(invalidBookObjectErrorMsg), status=400,mimetype='application/json')
+        return response
+    
 
 #GET /books/isbn
 @app.route('/books/<int:isbn>')
